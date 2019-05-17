97d6ed754b5a3c3de8758a29d39a7b0a8d578a11
Alexander Hitrov <alexander.hitrov@monetha.io>
2019-05-17 11:13:12 +0300

    d.ts copy

--- scripts/build-distribution.js
+++ scripts/build-distribution.js
@@ -5,6 +5,6 @@ shell.exec('./node_modules/.bin/tsc && tscpaths -p tsconfig.json -s ./src -o ./d
 const sourceDir = './src/contracts/';
 const destDir = './dist/contracts/';
 
-shell.cp(`${sourceDir}MonethaClaimHandler.d.ts`, `${destDir}MonethaClaimHandler.ts`);
-shell.cp(`${sourceDir}MonethaToken.d.ts`, `${destDir}MonethaToken.ts`);
-shell.cp(`${sourceDir}types.d.ts`, `${destDir}types.ts`);
+shell.cp(`${sourceDir}MonethaClaimHandler.d.ts`, destDir);
+shell.cp(`${sourceDir}MonethaToken.d.ts`, destDir);
+shell.cp(`${sourceDir}types.d.ts`, destDir);
